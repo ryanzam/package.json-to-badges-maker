d6e18bc0dadf1e66271a9ee4e7e408fb01cda701
ranjam <ryanzam2005@gmail.com>
2025-07-05 11:10:15 +0545

    remov workflows

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/package.json-to-badges-maker/'
 })
